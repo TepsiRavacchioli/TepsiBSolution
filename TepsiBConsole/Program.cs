77413ac716324d7f261b23a341e00bd14e76676f
Ermanno <Ermanno@PORTATILEERMANN>
2023-02-08 20:23:26 +0100

    converto 8 in base 2

--- TepsiBConsole/Program.cs
+++ TepsiBConsole/Program.cs
@@ -10,6 +10,15 @@ namespace TepsiBConsole
     {
         static void Main(string[] args)
         {
+            int num = 8;
+            string res = "";
+            while (num > 0)
+            {
+                int resto = num % 2;
+                res = resto + res;
+                num = num / 2;
+            }
+            Console.WriteLine($" risultato {res}");
         }
     }
 }
