760d104613a5e0f17daef2f31c0d6f0f13cc981f
Ermanno <Ermanno@PORTATILEERMANN>
2023-02-08 20:39:14 +0100

    numero dec da convertire e base in cui conv scelti da utente

--- TepsiBConsole/Program.cs
+++ TepsiBConsole/Program.cs
@@ -10,15 +10,20 @@ namespace TepsiBConsole
     {
         static void Main(string[] args)
         {
-            int num = 8;
+            int num = 0;
             string res = "";
+            int toBase = 0;
+            Console.WriteLine("inserisci un numero intero in base 10 ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Scegli la base in cui convertire il numero tra 2 e 16 ");
+            toBase = Convert.ToInt32(Console.ReadLine());
             while (num > 0)
             {
-                int resto = num % 2;
+                int resto = num % toBase;
                 res = resto + res;
-                num = num / 2;
+                num = num / toBase;
             }
-            Console.WriteLine($" risultato {res}");
+            Console.WriteLine($" risultato conversione di {num} in base {toBase}: {res}");
         }
     }
 }
