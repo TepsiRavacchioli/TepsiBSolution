0510fce5548dec6fc7a30186b0c384cbdfffedcb
Ermanno <Ermanno@PORTATILEERMANN>
2023-02-08 20:33:54 +0100

    converto 123 in exa

--- TepsiBConsole/Program.cs
+++ TepsiBConsole/Program.cs
@@ -10,13 +10,38 @@ namespace TepsiBConsole
     {
         static void Main(string[] args)
         {
-            int num = 8;
+            int num = 123;
             string res = "";
             while (num > 0)
             {
-                int resto = num % 8;
-                res = resto + res;
-                num = num / 8;
+                int resto = num % 16;
+                string alfa = "";
+                switch (resto)
+                {
+                    case 10:
+                        alfa = "A";
+                        break;
+                    case 11:
+                        alfa = "B";
+                        break;
+                    case 12:
+                        alfa = "C";
+                        break;
+                    case 13:
+                        alfa = "B";
+                        break;
+                    case 14:
+                        alfa = "E";
+                        break;
+                    case 15:
+                        alfa = "F";
+                        break;
+                    default:
+                        alfa = alfa +resto;
+                        break;
+                }
+                res = alfa + res;
+                num = num / 16;
             }
             Console.WriteLine($" risultato {res}");
         }
