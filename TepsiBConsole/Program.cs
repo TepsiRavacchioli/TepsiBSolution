a5597aa4c253eed090691a78f8fae179ef8a0555
Ermanno <Ermanno@PORTATILEERMANN>
2023-02-08 19:17:47 +0100

    Add project files.

---
+++ TepsiBConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TepsiBConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
